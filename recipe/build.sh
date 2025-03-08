d2ae696a0493f24675d43484cbd4edd7924f5ed3
Flavio Martins <flavio.f.martins@tecnico.ulisboa.pt>
2025-03-08 00:48:55 +0000

    Skip tests on osx_arm64

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,8 +5,10 @@ set -ex
 # compile
 make -j$CPU_COUNT
 
-# test
-make quicktest
+# test skipped on osx_arm64
+if [[ "${target_platform}" != osx-arm64 ]]; then
+    make quicktest
+fi
 
 # install (cannot use 'make install')
 mkdir -p $PREFIX/bin
